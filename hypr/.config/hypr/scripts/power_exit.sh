2f2af327418732b91837c5939a535b3eb8846c4a
melih-ucgun <melihucgun@gmail.com>
2025-12-27 16:19:12 +0300

    güç menüsü güncellendi

--- hypr/.config/hypr/scripts/power_exit.sh
+++ hypr/.config/hypr/scripts/power_exit.sh
@@ -4,19 +4,29 @@
 options="󰐥 Kapat\n󰜉 Yeniden Başlat\n󰤄 Uyku\n󰍃 Oturumu Kapat\n󰷛 Kilitle"
 
 # Rofi ile menüyü aç
+# Eğer bir önceki adımda oluşturduğumuz Tokyo Night temasını kullanmak istersen:
+# -theme-str 'window {width: 300px;}' kısmını silip yerine:
+# -theme ~/.config/rofi/tokyonight_powermenu.rasi yazabilirsin.
 chosen=$(echo -e "$options" | rofi -dmenu -i -p "Sistem İşlemi" -theme-str 'window {width: 300px;}')
 
 # Seçime göre işlemi yap
 case $chosen in
-    *"Kapat"*)
-        systemctl poweroff ;;
-    *"Yeniden Başlat"*)
-        reboot ;;
-    *"Uyku"*)
-        systemctl suspend ;;
-    *"Oturumu Kapat"*)
-        hyprctl dispatch exit ;;
-    *"Kilitle"*)
-        # Eğer hyprlock yüklü değilse 'swaylock' veya 'gtklock' yazabilirsin
-        hyprlock ;; 
+*"Kapat"*)
+  systemctl poweroff
+  ;;
+*"Yeniden Başlat"*)
+  systemctl reboot
+  ;;
+*"Uyku"*)
+  # Önce hyprlock çalışıyor mu kontrol et, çalışmıyorsa çalıştır ve sonra uykuya dal
+  pidof hyprlock || hyprlock &
+  sleep 0.5 && systemctl suspend
+  ;;
+*"Oturumu Kapat"*)
+  hyprctl dispatch exit
+  ;;
+*"Kilitle"*)
+  # Eğer zaten kilitliyse tekrar komut gönderip hataya düşmesini engeller
+  pidof hyprlock || hyprlock
+  ;;
 esac
